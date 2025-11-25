adddffe29a180440ae737ba3437bd1c90d930b77
unknown <rabin.khadka0@gmail.com>
2025-11-25 15:36:41 -0500

    Fix

--- simplesamlphp/public/function.php
+++ simplesamlphp/public/function.php
@@ -1,17 +1,19 @@
 <?php
-if(!function_exists('logSSOFlow')){
-function logSSOFlow($message, $logFile = '/log/sso_flow_log.log') {
-    // Ensure the logs directory exists
-    if (!is_dir(dirname($logFile))) {
-        mkdir(dirname($logFile), 0755, true);
+if (!function_exists('logSSOFlow')) {
+    function logSSOFlow($message) {
+
+        // Correct log file path
+        $logFile = __DIR__ . '/../log/sso_flow_log.log';
+
+        // Ensure log directory exists
+        if (!is_dir(dirname($logFile))) {
+            mkdir(dirname($logFile), 0755, true);
+        }
+
+        $timestamp = date('Y-m-d H:i:s');
+        $logMessage = "[$timestamp] $message" . PHP_EOL;
+
+        file_put_contents($logFile, $logMessage, FILE_APPEND | LOCK_EX);
     }
-
-    // Add a timestamp to the log message
-    $timestamp = date('Y-m-d H:i:s');
-    $logMessage = "[$timestamp] $message" . PHP_EOL;
-
-    // Write the log message to the file
-    file_put_contents($logFile, $logMessage, FILE_APPEND | LOCK_EX);
-}
 }
 ?>
