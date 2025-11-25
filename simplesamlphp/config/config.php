bad104d4638f2bd31607b1ac7d96b0984ea35594
unknown <rabin.khadka0@gmail.com>
2025-11-25 08:15:11 -0500

    Updated basepathurl on config.php

--- simplesamlphp/config/config.php
+++ simplesamlphp/config/config.php
@@ -31,7 +31,7 @@ $config = [
      * external url, no matter where you come from (direct access or via the
      * reverse proxy).
      */
-    'baseurlpath' => 'https://buildprocure.com/simplesamlphp/',
+    'baseurlpath' => 'https://buildprocure.com/simplesaml/',
     'tempdir' => '/tmp/simplesaml',
 
     /*
